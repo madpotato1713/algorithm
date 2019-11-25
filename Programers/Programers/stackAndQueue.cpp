27290cf545a3e9815db265c3e236f0cd4c706542
madpotato1713 <madpotato1713@gmail.com>
2019-11-26 01:46:02 +0900

    스택/큐 다른 풀이

--- Programers/Programers/stackAndQueue.cpp
+++ Programers/Programers/stackAndQueue.cpp
@@ -1,20 +1,44 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<int> prices) {
+//	vector<int> answer;
+//	int size = prices.size();
+//
+//	for (int i = 0; i < size; i++) {
+//		int cnt = 0;
+//		for (int j = i + 1; j < size; j++) {
+//			cnt++;
+//			if (prices[i] > prices[j]) break;
+//		}
+//		answer.push_back(cnt);
+//	}
+//
+//	return answer;
+//}
+
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-	vector<int> answer;
+	vector<int> answer(prices.size());
+	stack<int> s;
 	int size = prices.size();
-
-	for (int i = 0; i < size; i++) {
-		int cnt = 0;
-		for (int j = i + 1; j < size; j++) {
-			cnt++;
-			if (prices[i] > prices[j]) break;
+	for (int i = 0;i < size;i++) {
+		while (!s.empty() && prices[s.top()] > prices[i]) {
+			answer[s.top()] = i - s.top();
+			s.pop();
 		}
-		answer.push_back(cnt);
+		s.push(i);
 	}
-
+	while (!s.empty()) {
+		answer[s.top()] = size - s.top() - 1;
+		s.pop();
+	}
 	return answer;
 }
