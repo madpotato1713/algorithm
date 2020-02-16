d60cb045e762ba0a5e7a276d92b32a5dd4187bdb
madpotato1713 <madpotato1713@gmail.com>
2020-02-16 14:52:42 +0900

    125. Valid Palindrome

--- LeetCode/LeetCode/BestTimetoBuyandSellStockII.cpp
+++ LeetCode/LeetCode/BestTimetoBuyandSellStockII.cpp
@@ -1,32 +1,60 @@
+/* CASE1 */
 class Solution {
 public:
-	int maxProfit(vector<int>& prices) {
-		int res = 0;
-		int sum = 0;
-
-		int size = prices.size();
-		if (!size) return res;
-
-		int fst, snd;
-		fst = snd = prices[0];
-
-		for (int i = 1; i < size; i++) {
-			if (snd > prices[i]) {
-				snd = prices[i];
+	bool isPalindrome(string s) {
+		string str;
+		for (int i = 0; i < s.size(); i++) {
+			char ch = s[i];
+			if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+				str += tolower(ch);
 			}
-
-			if (fst > prices[i]) {
-				fst = prices[i];
-				sum += res;
-				res = 0;
-			}
-			else {
-				res = max(prices[i] - fst, res + prices[i] - snd);
-				snd = prices[i];
+			else if (ch >= '0' && ch <= '9') {
+				str += ch;
 			}
 		}
-		if (res) sum += res;
 
-		return sum;
+		int left = 0;
+		int right = str.size() - 1;
+
+		while (left < right) {
+			if (str[left] != str[right])
+				return false;
+
+			left++;
+			right--;
+		}
+
+		return true;
 	}
 };
+
+/* CASE2 */
+class Solution {
+public:
+	bool isPalindrome(string s) {
+		int left = 0;
+		int right = s.size() - 1;
+
+		while (left < right) {
+			char lc = tolower(s[left]);
+			char rc = tolower(s[right]);
+
+
+			if (!((lc >= 'a' && lc <= 'z') || (lc >= '0' && lc <= '9'))) {
+				left++;
+			}
+			else if (!((rc >= 'a' && rc <= 'z') || (rc >= '0' && rc <= '9'))) {
+				right--;
+			}
+			else {
+				if (lc != rc)
+					return false;
+				left++;
+				right--;
+			}
+		}
+
+		return true;
+	}
+};
+
