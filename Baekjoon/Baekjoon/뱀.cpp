1898450bb9a25bdea5f6346fe983d802ffd7dc0b
madpotato1713 <madpotato1713@gmail.com>
2020-04-08 00:12:42 +0900

    387. First Unique Character in a String

--- Baekjoon/Baekjoon/뱀.cpp
+++ Baekjoon/Baekjoon/뱀.cpp
@@ -18,12 +18,34 @@ struct point {
 int map[MAXIMUM][MAXIMUM];
 int N;	//보드의 크기
 int K;	//사과의 개수
-int L;	//뱀의 변환 정보
+int L;	//위치 변환 정보
 //vector<point> apples;	//사과 위치 저장
 vector<point> snake;	//뱀 위치 정보 저장
 //char snakedir;
 queue<pair<int, char>> moveinfo;	//변환정보 저장(L, D)
 int direction[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };	//상, 하, 좌, 우
+
+int dirInfo(int snakedir, char lr) {
+	//다음 방향 정보 변경
+	if (snakedir == 0) {
+		if (lr == 'L') snakedir = 2;
+		else if (lr == 'D') snakedir = 3;
+	}
+	else if (snakedir == 1) {
+		if (lr == 'L') snakedir = 3;
+		else if (lr == 'D') snakedir = 2;
+	}
+	else if (snakedir == 2) {
+		if (lr == 'L') snakedir = 1;
+		else if (lr == 'D') snakedir = 0;
+	}
+	else if (snakedir == 3) {
+		if (lr == 'L') snakedir = 0;
+		else if (lr == 'D') snakedir = 1;
+	}
+
+	return snakedir;
+}
 
 int play() {
 	int cnt = 0;
@@ -34,45 +56,39 @@ int play() {
 
 		//저장된 sec만큼 이동
 		for (int i = 0; i < front.first; i++) {
-			//int snakeLen = snake.size() - 1;
-			//int headY = snake[0].y, headX = snake[0].x;
-			//int tailY = snake[snakeLen].y, tailX = snake[snakeLen].x;
+			cnt++;
+			int nextDir = snake[snake.size() - 1].dir;
+			for (int s = snake.size() - 1; s >= 0; s--) {
+				int nextY = snake[s].y + direction[snake[s].dir][0];
+				int nextX = snake[s].x + direction[snake[s].dir][1];
 
-			//int nextY = headY + direction[snakedir][0], nextX = headX + direction[snakedir][1];
+				//벽에 부딪히는 경우(끝) 수정 필요(맨 머리만 검사하면 될 것 같다)
+				if (nextY < 1 || nextY > N || nextX < 1 || nextX > N)
+					return cnt;
 
-			//이동하는 경로에 사과(-1)가 있는 경우
+				////자기 자신한테 부딪히는 경우(끝)	수정 필요(맨 머리만 검사하면 될 것 같다)
+				//for (int c = 0; c < snake.size(); c++) {
+				//	if (snake[c].y == nextY && snake[c].x == nextX) {
+				//		return cnt;
+				//	}
+				//}
 
-			//이동하는 경우에 끝(벽 또는 자기 자신과 마주쳤을 때)
-
-			//일반 이동
-			//바로 다음 몸통의 위치정보를 이어받음
-
-			//map[tailY][tailX] = 0;	//꼬리 위치 갱신
-			//map[headY][headX] = 1;	//머리 위치 갱신
-			//for (auto s : snake) {	//뱀 위치 갱신
-			//	s.y += direction[snakedir][0];
-			//	s.x += direction[snakedir][1];
-			//}
+				//사과 있는 경우
+				if (map[nextY][nextX] == -1) {
+					snake.push_back(point(nextY, nextX, snake[s].dir));
+					map[nextY][nextX] = 0;
+				}
+				//사과 없는 경우
+				else {
+					snake[s].y = nextY;
+					snake[s].x = nextX;
+				}
+				int tmpDir = snake[s].dir;
+				snake[s].dir = nextDir;
+				nextDir = tmpDir;
+			}
 		}
-
-		//다음 방향 정보 변경
-		/*if (snakedir == 0) {
-			if (front.second == 'L') snakedir = 2;
-			else if (front.second == 'D') snakedir = 3;
-		}
-
-		else if (snakedir == 1) {
-			if (front.second == 'L') snakedir = 3;
-			else if (front.second == 'D') snakedir = 2;
-		}
-		else if (snakedir == 2) {
-			if (front.second == 'L') snakedir = 1;
-			else if (front.second == 'D') snakedir = 0;
-		}
-		else if (snakedir == 3) {
-			if (front.second == 'L') snakedir = 0;
-			else if (front.second == 'D') snakedir = 1;
-		}*/
+		snake[snake.size() - 1].dir = dirInfo(snake[snake.size() - 1].dir, front.second);
 	}
 
 	return cnt;
@@ -97,7 +113,7 @@ int main() {
 	}
 	//뱀의 초기 위치와 방향 저장 : (1, 1)부터 시작, 처음에 오른쪽을 향함
 	snake.push_back(point(1, 1, 3));
-	map[1][1] = 1;	//뱀의 초기 위치 표시
+	//map[1][1] = 1;	//뱀의 초기 위치 표시
 	//snakedir = 3;	//뱀의 초기 방향은 오른쪽
 
 	cout << play() << endl;
